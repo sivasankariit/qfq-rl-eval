cf6b9d5bc876c5559087cc06a16a37bc45b2a9ae
Vimalkumar <j.vimal@gmail.com>
2013-01-26 23:52:51 -0800

    Throughput test with iperf

---
+++ tests/run-xput.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+dir=`date +%b%d-%H:%M`
+time=40
+ns=0
+
+function finish {
+    killall -9 ssh
+    exit
+}
+
+trap finish SIGINT
+
+mkdir -p $dir
+for rl in qfq; do
+for nrls in 1; do
+for rate in 1000 3000 5000 7000 9000; do
+    exptid=rl-$rl-nrls-$nrls-rate-$rate
+    python netperf.py --nrr 0 \
+        --exptid $exptid \
+        -t $time \
+        --rl $rl \
+        --rate $rate \
+        --nrls $nrls \
+	--ns $nrls # Same num of senders as rate limiters
+
+    mv $exptid.tar.gz $dir/
+
+    pushd $dir;
+    tar xf $exptid.tar.gz
+    #python ../plot.py --rr $exptid/* -o $exptid.png --ymin 0.9
+    popd;
+done;
+done;
+done;
+
+echo Experiment results are in $dir
