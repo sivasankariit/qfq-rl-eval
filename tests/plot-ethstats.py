a056fbec432ce8990ae8e65ec1314863a8cba2e6
Vimalkumar <j.vimal@gmail.com>
2013-02-22 23:05:27 -0800

    Move output parsers to separate file.

--- tests/plot-ethstats.py
+++ tests/plot-ethstats.py
@@ -14,6 +14,7 @@ import plot_defaults
 import sys
 import itertools
 from helper import *
+from parser import ESParser
 
 IFACE="eth2"
 ZERO="0.00"
@@ -28,34 +29,6 @@ args = parser.parse_args()
 rspaces = re.compile(r'\s+')
 
 plot_defaults.rcParams['figure.figsize'] = 4, 3.5
-
-class ESParser:
-    def __init__(self, filename):
-        self.f = filename
-        self.lines = open(filename).readlines()
-        self.parse()
-
-    def parse_line(self, line):
-        iface, rest = line.split(":")
-        iface = iface.strip()
-        data = rspaces.split(rest.strip())
-        return {"iface": iface, "in":data[0], "out":data[3]}
-
-    def parse(self):
-        ret = dict()
-        util = []
-        for line in self.lines:
-            d = self.parse_line(line)
-            if d["iface"] == IFACE and d["out"] != ZERO:
-                util.append(float(d["out"]))
-        L = len(util)
-        L = L/3
-        util = util[L/10:-L/10]
-        self.util = util
-        return util
-
-    def summary(self):
-        return dict(mean=mean(self.util), stdev=stdev(self.util))
 
 nrls = [1, 10, 100, 1000]
 rates = [1000, 3000, 5000, 7000, 9000]
