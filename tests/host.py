8a3711501802fd039c9fdfe09eac3563851fcb74
Vimalkumar <j.vimal@gmail.com>
2013-02-24 18:36:38 -0800

    bugfix: hash table can only have 256 entries.  Perhaps they can be chained?

--- tests/host.py
+++ tests/host.py
@@ -197,6 +197,7 @@ class Host(object):
         self.cmd(c)
 
     def add_htb_hash(self, num_hash_bits=4):
+        num_hash_bits = min(8, num_hash_bits)
         num_hash = 1 << num_hash_bits
         self.num_hash = num_hash
         self.hash_mask = hex((1 << num_hash_bits) - 1)
@@ -212,9 +213,10 @@ class Host(object):
         c += "tc filter add dev %s protocol all parent 1: prio 1 u32 ht 2:%d: match ip dport %d %d flowid 1:%d" % (dev, hash, port, self.hash_mask, klass)
         self.cmd(c)
 
-    def add_n_htb_class(self, rate='5Gbit', ceil='5Gbit', start_port=1000, num_hash=8):
+    def add_n_htb_class(self, rate='5Gbit', ceil='5Gbit', start_port=1000, num_class=8):
+        num_hash = self.num_hash
         dev = self.get_10g_dev()
-        c  = "for klass in `seq %s %s`; do " % (start_port, start_port + self.num_hash)
+        c  = "for klass in `seq %s %s`; do " % (start_port, start_port + num_class)
         c += "  hexclass=`perl -e \"printf('%%x', $klass %% %s)\"`; " % (num_hash)
         c += "  tc filter add dev %s protocol all parent 1: prio 1 u32 ht 2:$hexclass: match ip dport $klass %s flowid 1:%s; " % (dev, self.hash_mask, "$klass")
         c += "  tc class add dev %s classid 1:%s parent 1: htb rate %s ceil %s; " % (dev, "$klass", rate, ceil)
@@ -351,13 +353,17 @@ class Host(object):
         self.cmd_async(cmd)
         return
 
-    def start_n_udp(self, nclass, nprogs, dest, startport, rate=10000, burst=(1 << 22)):
+    def start_n_udp(self, nclass, nprogs, dest, startport, rate=10000, burst=(1 << 22), dir=None):
         # Start nprogs udp traffic sources, nclass per each program,
         # starting with @startport.  I assume each destination port is
         # one class.
+        self.cmd_async("unlimit -n 1024000")
         while nprogs:
             nprogs -= 1
-            cmd = "%s %s %s %s %s %s > /dev/null" % (UDP, dest, startport, nclass, rate, burst)
+            outfile = '%s/udp-%d.txt' % (dir, nprogs)
+            if dir is None:
+                outfile = '/dev/null'
+            cmd = "%s %s %s %s %s %s > %s 2>&1" % (UDP, dest, startport, nclass, rate, burst, outfile)
             self.cmd_async(cmd)
         return
 
