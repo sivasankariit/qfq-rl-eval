333a274ce9b0a445147ab3413a70b416349cb40f
Vimalkumar <j.vimal@gmail.com>
2013-02-26 18:21:01 -0800

    Make plot script slightly more robust to missing data.

--- tests/hotcloud13/plot-compare.py
+++ tests/hotcloud13/plot-compare.py
@@ -43,6 +43,7 @@ rspaces = re.compile(r'\s+')
 plot_defaults.rcParams['figure.figsize'] = 4, 3.5
 
 rls = ["none", "htb", "eyeq"]#, "tbf"]
+rls_seen = []
 rl_name = dict(none="app", htb="htb",eyeq="eyeq")
 colour_rl = dict(none="yellow", htb="green", tbf="blue", eyeq="grey")
 
@@ -66,6 +67,10 @@ for (i,rl), (j,rate) in itertools.product(E(rls), E(rates)):
     ys = []
     for run in runs:
         dir = DIR(rl, rate, run)
+        fs_dir = os.path.join(args.dir, dir)
+        if not os.path.exists(fs_dir):
+            print dir, "does not exist; ignoring..."
+            continue
         ethstats_fname = os.path.join(args.dir, dir, "net.txt")
         mpstat_fname = os.path.join(args.dir, dir, "mpstat.txt")
 
@@ -77,13 +82,18 @@ for (i,rl), (j,rate) in itertools.product(E(rls), E(rates)):
         print rl, rate, summ, run, mpstats.summary()
         ys.append(mpstats.kernel())
 
+    if len(ys) == 0:
+        continue
+
     x = j * (len(rls) + 1) + i
     bar = plt.bar(x, mean(ys), width=1, color=get_rl_colour(rl),
                   yerr=stdev(ys), ecolor='red')
     rl_bar[rl] = bar[0]
+    if rl not in rls_seen:
+        rls_seen.append(rl)
 
-plt.legend([rl_bar[rl] for rl in rls],
-           [rl_name[rl] for rl in rls],
+plt.legend([rl_bar[rl] for rl in rls_seen],
+           [rl_name[rl] for rl in rls_seen],
            loc="upper left")
 width = len(rls) + 1
 xtickloc = width * numpy.arange(len(rates)) + (width / 2)
@@ -95,4 +105,3 @@ plt.xlabel("Rates in Mb/s")
 if args.out:
     plt.savefig(args.out)
     print "saved to", args.out
-
