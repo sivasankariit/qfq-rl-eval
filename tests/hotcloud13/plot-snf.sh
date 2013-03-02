27060055cf406beaf2cb3608ea73413043b41d7d
Sivasankar Radhakrishnan <sivasankar@cs.ucsd.edu>
2013-03-02 10:36:49 +0000

    Support sniffer plots for multiple traffic classes

--- tests/hotcloud13/plot-snf.sh
+++ tests/hotcloud13/plot-snf.sh
@@ -8,6 +8,7 @@ fi
 dir=$1
 
 EXPT_RATES=`python site_config.py --var EXPT_RATES`
+EXPT_NCLASSES=`python site_config.py --var EXPT_NCLASSES`
 EXPT_NRL=`python site_config.py --var EXPT_NRL`
 EXPT_RL=`python site_config.py --var EXPT_RL`
 EXPT_RUN=`python site_config.py --var EXPT_RUN`
@@ -19,8 +20,9 @@ echo TMPDIR=$TMPDIR
 for rate in $EXPT_RATES; do
 for nrls in $EXPT_NRL; do
 for rl in $EXPT_RL; do
+for nclasses in $EXPT_NCLASSES; do
 for run in $EXPT_RUN; do
-    exptid=rl-$rl-nrls-$nrls-rate-$rate-run-$run
+    exptid=rl-$rl-nrls-$nrls-rate-$rate-ncl-$nclasses-run-$run
     echo "Processing $dir/$exptid"
     echo "    Extracting tar files"
     tar xf $dir/$exptid.tar.gz -C $TMPDIR
@@ -31,6 +33,7 @@ for run in $EXPT_RUN; do
         -o $dir/$exptid/pkt_arr.pdf > $dir/$exptid/plot-sniffer-output.txt
     echo "    Removing untar'd sniffer file"
     rm $TMPDIR/$exptid/pkt_snf.txt
+done;
 done;
 done;
 done;
