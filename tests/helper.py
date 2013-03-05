423ff75a3dc33bdcf1759dc6f9d15170791029df
Vimalkumar <j.vimal@gmail.com>
2013-03-05 10:58:20 -0800

    Guard argument to sqrt and ensure it's > 0

--- tests/helper.py
+++ tests/helper.py
@@ -40,7 +40,13 @@ def mean(l):
         return 0
     return sum(l) * 1.0 / len(l)
 
-def stdev(l):
-    m = mean(l)
+def stdev(l, m=None):
+    """
+    @m is another mean with which we can measure stdev against.  Since
+    this offers no guarantee that sq > m*m, I added an abs() before
+    computing sqrt.
+    """
+    if m is None:
+        m = mean(l)
     sq = mean(map(lambda e: e * e, l))
-    return math.sqrt(sq - m * m)
+    return math.sqrt(abs(sq - m * m))
