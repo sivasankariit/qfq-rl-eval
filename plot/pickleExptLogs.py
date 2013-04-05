20385ec3aae3416221343ea40e5250f1b85b2fa5
Sivasankar Radhakrishnan <sivasankar@cs.ucsd.edu>
2013-04-05 11:31:29 +0000

    Script to pickle experiment logs for plotting
    Currently only pickles burst length in packets

---
+++ plot/pickleExptLogs.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import cPickle
+import os
+import random
+import sys
+import tarfile
+
+from SnifferParser import SnifferParser
+
+
+# Used to load data from a picked file to variables
+def readPickledFile(infile):
+    fd = open(infile, 'rb')
+    data = cPickle.load(fd)
+    fd.close()
+    return data
+
+
+def pickleSnfFile(snf_file, pickle_dir, max_lines=100000):
+
+    # Parse the sniffer log file
+    sniff = SnifferParser(snf_file, max_lines=max_lines)
+
+    # Pickle burstlen_pkt data
+    burstlen_pkt_pfile = os.path.join(pickle_dir, 'burstlen_pkt.txt')
+    burstlen_pkt = sniff.get_burstlen_pkt()
+    summary = sniff.summary_burstlen_pkt()
+    data = (burstlen_pkt, summary)
+    fd = open(burstlen_pkt_pfile, 'wb')
+    cPickle.dump(data, fd)
+    fd.close()
+
+
+def main(argv):
+
+    if len(argv) != 3:
+        print 'Usage: ', argv[0], 'expt_dir tmp_dir'
+        sys.exit(-1)
+
+    expt_dir = argv[1]
+    tmp_dir = argv[2]
+
+    # Temp directory to extract the sniffer data and pickle it
+    snf_data_dir = os.path.join(tmp_dir, 'snf_data')
+    os.makedirs(snf_data_dir)
+
+    # Extract the sniffer data to the temp directory
+    snf_tarfile = os.path.join(expt_dir, 'logs/pkt_snf.tar.gz')
+    tar = tarfile.open(snf_tarfile)
+    tar.extractall(snf_data_dir)
+    tar.close()
+
+    # Pickle sniffer data
+    pickleSnfFile(os.path.join(snf_data_dir, 'pkt_snf.txt'),
+                  os.path.join(expt_dir, 'pickled'),
+                  max_lines = 1000000)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
