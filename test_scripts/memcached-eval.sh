aa5e59d818311a6729641c25989f160b91a65537
Sivasankar Radhakrishnan <sivasankar@cs.ucsd.edu>
2013-07-26 18:58:37 +0000

    Minor changes to mcperf high level wrapper

--- test_scripts/memcached-eval.sh
+++ test_scripts/memcached-eval.sh
@@ -20,7 +20,8 @@ touch $dir/expt_config.txt
 echo "NUM_CPUS = ${NUM_CPUS}" >> $dir/expt_config.txt
 echo "EXCLUDE_CPUS = ${EXCLUDE_CPUS}" >> $dir/expt_config.txt
 mc_pair_rate=500
-for mcrate in 5000 7000 9000 11000; do
+mcsize=4096
+for mcrate in 1000 3000 5000 7000; do
 for rl in none htb qfq; do
 for mctenants in 8 15; do
 for run in 1; do
@@ -34,13 +35,14 @@ for run in 1; do
     chmod a+w expsift_tags
     echo "link_speed_mbps=10000" >> expsift_tags
     echo "nic_vendor=intel" >> expsift_tags
-    echo "workload=memcached_set" >> expsift_tags
+    echo "workload=memcached_get" >> expsift_tags
     echo "mtu=$mtu" >> expsift_tags
     echo "tso=off" >> expsift_tags
     echo "gso=off" >> expsift_tags
     echo "lro=on" >> expsift_tags
     echo "gro=off" >> expsift_tags
     echo "rl=$rl" >> expsift_tags
+    echo "mcsize=$mcsize" >> expsift_tags
     echo "mc_pair_rate=$mc_pair_rate" >> expsift_tags
     echo "mcrate=$mcrate" >> expsift_tags
     echo "mctenants=$mctenants" >> expsift_tags
@@ -53,10 +55,11 @@ for run in 1; do
         --rl $rl \
         --mc_pair_rate $mc_pair_rate \
         --mcrate $mcrate \
-        --mcworkload "set" \
+        --mcworkload "get" \
         --mtu $mtu \
         --htb-mtu $mtu \
         --mctenants $mctenants \
+        --mcsize $mcsize \
         --outdir $dir/$exptid
 
     chmod a+w $dir/$exptid
