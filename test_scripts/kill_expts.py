b76e235a9d61340d63a67547b014db5e3e2c6eea
Sivasankar Radhakrishnan <sivasankar@cs.ucsd.edu>
2013-08-16 22:15:07 +0000

    Script to kill rate limiting experiments

---
+++ test_scripts/kill_expts.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import sys
+import argparse
+import termcolor as T
+from time import sleep
+from host import *
+from site_config import *
+import os
+
+parser = argparse.ArgumentParser(description="Kill rate limiting expts")
+
+parser.add_argument('--servers',
+                    dest="servers",
+                    metavar='SERVER',
+                    help="Memcached servers to run tests",
+                    nargs="+", default=config['DEFAULT_MC_SERVERS'])
+
+parser.add_argument('--clients',
+                    dest="clients",
+                    metavar='CLIENT',
+                    help="Memcached clients to run tests",
+                    nargs="+", default=config['DEFAULT_MC_CLIENTS'])
+
+args = parser.parse_args()
+
+
+hlist = HostList()
+
+print T.colored("Servers:---", "green")
+for ip in args.servers:
+    h = Host(ip)
+    hlist.append(h)
+    print T.colored(ip, "green")
+
+print T.colored("Clients:---", "yellow")
+for ip in args.clients:
+    h = Host(ip)
+    hlist.append(h)
+    print T.colored(ip, "yellow")
+
+hlist.remove_qdiscs()
+hlist.stop_mpstat()
+hlist.killall("memcached mcperf")
+hlist.stop_trafgen()
+hlist.rmmod_qfq()
+hlist.clear_intel_hw_rate_limits(config['NIC_HW_QUEUES'])
+hlist.clear_mellanox_hw_rate_limits()
